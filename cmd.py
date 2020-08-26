0a2dfc584f547c843dd849c79178a9dae8ae607e
Steve Thao <ntthao2606@gmail.com>
2020-08-26 20:50:13 +0700

    Create cmd.py

---
+++ cmd.py
@@ -0,0 +1,7 @@
+print("Hello Thao")
+print("Tui la UIUX Designer")
+
+# hướng dẫn chạy file python trg cmd
+print("Bước 1: gõ E:")
+print("Bước 2: nhập đường dẫn E:\Python\Practice")
+print("Bước 3: nhập tên file")
