e65590f4ac06dc239a6bf54a14d263cd266e9cb1
Steve Thao <ntthao2606@gmail.com>
2020-08-26 20:50:18 +0700

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,25 @@
+k = 10092016
+b = 10092017
+s = k + b
+print(s)
+
+# từ khóa python
+print("- Tên biến ko dc trùng với từ khóa của python")
+print("- Tên biến chỉ chứa các chữ cái, số, dấu '_', có phân biệt chữ hoa & chữ thường")
+
+# ví dụ
+tuoi = 26
+ten = "Nguyen Thanh Thao"
+PI = 3.14
+print(tuoi)
+print(ten)
+print(PI)
+
+# khai báo nhiều biến cùng lúc
+# <tên biến 1>, <tên biến 2>, <tên biến 3>, ..., <tên bién n> = <giá trị biến 1>, <giá trị biến 2>, <giá trị biến 3>, ..., <giá trị biến n>
+
+# kiểm tra kiểu dữ liệu của biến
+# type(<tên biến>)
+print(type(tuoi))
+print(type(ten))
+print(type(PI))
